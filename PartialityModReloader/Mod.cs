b96246164c84a953b00d11c7e7398960d9246176
Julien Opoix <laymain@gmail.com>
2020-03-12 02:45:21 +0100

    Mod skeleton

---
+++ PartialityModReloader/Mod.cs
@@ -0,0 +1,14 @@
+using Partiality.Modloader;
+
+namespace PartialityModReloader
+{
+    public class Mod : PartialityMod
+    {
+        public Mod()
+        {
+            ModID = "PartialityModReloader";
+            Version = "0.0.0";
+            author = "Laymain";
+        }
+    }
+}
