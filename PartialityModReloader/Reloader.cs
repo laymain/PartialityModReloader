b679bdc96403c6bc0a7fe794906c8a691384cedc
Julien Opoix <laymain@gmail.com>
2020-04-14 02:50:03 +0200

    Reloader implementation that monitor file changes and update reloadable methods

---
+++ PartialityModReloader/Reloader.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using PartialityModReloader.IO;
+
+namespace PartialityModReloader
+{
+    public class Reloader : IDisposable
+    {
+        private const BindingFlags AllBindings = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetField |
+                                                 BindingFlags.SetField | BindingFlags.GetProperty | BindingFlags.SetProperty;
+
+        private readonly Dictionary<string, long> _reloadableMethods = new Dictionary<string, long>();
+        private readonly DelayedFileSystemChangeWatcher _watcher;
+
+        public Reloader(string folder)
+        {
+            BuildCache(folder);
+            _watcher = new DelayedFileSystemChangeWatcher(TimeSpan.FromSeconds(3))
+            {
+                Path = folder,
+                Filter = "*.dll"
+            };
+            _watcher.Changed += (sender, args) => ReloadMethods(args.FullPath);
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        private void BuildCache(string folder)
+        {
+            foreach (string filepath in Directory.GetFiles(folder, "*.dll"))
+            foreach (MethodInfo method in ReadReloadableMethods(filepath))
+            {
+                _reloadableMethods.Add(method.GetKey(), method.GetAddress());
+                Console.WriteLine($"Reloadable method found: {method.GetKey()}");
+            }
+        }
+
+        private static IEnumerable<MethodInfo> ReadReloadableMethods(string filepath)
+        {
+            try
+            {
+                return
+                    from type in Assembly.Load(File.ReadAllBytes(filepath)).GetTypes()
+                    from method in type.GetMethods(AllBindings)
+                    where method?.MethodImplementationFlags == MethodImplAttributes.NoInlining
+                    select method;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error while analysing file '{filepath}': {e.Message}");
+                return Enumerable.Empty<MethodInfo>();
+            }
+        }
+
+        private void ReloadMethods(string filepath)
+        {
+            Console.WriteLine($"File changed: {filepath}");
+            try
+            {
+                foreach (MethodInfo method in ReadReloadableMethods(filepath))
+                {
+                    if (_reloadableMethods.TryGetValue(method.GetKey(), out long currentAddress))
+                    {
+                        Memory.WriteJump(currentAddress, method.GetAddress());
+                        Console.WriteLine($"Reloadable method updated: {method.GetKey()}");
+                    }
+                    else
+                    {
+                        _reloadableMethods.Add(method.GetKey(), method.GetAddress());
+                        Console.WriteLine($"New reloadable method found: {method.GetKey()}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error while reloading file '{filepath}': {e.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            _watcher?.Dispose();
+        }
+    }
+
+    internal static class MethodInfoExtensions
+    {
+        public static string GetKey(this MethodInfo method)
+        {
+            return $"{method.DeclaringType?.FullName}::{method.Name}";
+        }
+
+        public static long GetAddress(this MethodInfo methodInfo)
+        {
+            long address = Memory.GetMethodStart(methodInfo, out Exception e);
+            if (e != null) throw e;
+            return address;
+        }
+    }
+}
